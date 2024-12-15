318e0c5e4d68e6e766f2dea235285fa43a78b0ec
ssfxx0923 <ssfxx0923@gmail.com>
2024-12-15 22:10:27 +0800

    Initial commit

--- api/index.js
+++ api/index.js
@@ -80,7 +80,7 @@ const API_CONFIG = {
                 },
                 ...messages
             ],
-            model: "grok-2-vision-1212",
+            model: "grok-3-vision-1212",
             stream: true,
             temperature: 0.7
         }),
